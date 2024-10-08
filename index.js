c6dcb43b0699f8d80230b3617f2d095ba0a27558
Oleg <47694131+Oleg-Freeman@users.noreply.github.com>
2024-10-08 13:47:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
     const https = require('https');
     const options = {
-        host: 'jsonplaceholder.typicode.com',
-        path: '/todos/1',
+        // host: 'jsonplaceholder.typicode.com',
+        host: 'https://5995-91-196-55-8.ngrok-free.app'
+        //path: '/todos/1',
+        path: '/status    
         method: 'GET',
     };
     const req = https.request(options, (res) => {
